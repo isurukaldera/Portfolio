ba8385a2142b224cbb035caa79501ae48227b450
isuru kaldera <isuru.kaldera6425@gmail.com>
2026-01-04 23:45:19 +0000

    Tags

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -59,7 +59,7 @@ const projectImages = [
     title: "FOOD DELIVERY APP",
     description: "Android-based food delivery application developed using Java and Firebase",
     imageSrc: "/logo/project-6.png",
-    tags: ["AWS", "Docker", "Kubernetes", "Terraform", "CI/CD"],
+    tags: ["Android Studio", "Java"],
     github: "https://github.com/isurukaldera/Android-Application-Food-Deilivry-App-?tab=readme-ov-file",
     live: "#",
     color: "from-indigo-600 to-cyan-400"
